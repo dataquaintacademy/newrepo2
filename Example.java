191db2565bba6ccb60d399a77af54e57cfe480a7
dataquaintacademy <dataquaintacademy@gmail.com>
2026-05-26 10:56:48 +0530

    Add message for GitHub administrator

--- Example.java
+++ Example.java
@@ -5,5 +5,7 @@ public static void main(String[] args)
 System.out.println("Hello world");
 System.out.println("welcome to the world of DevOPs");
 System.out.println("I'm new to to development");
+  System.out.println("I'm github administrator");
+  
 }
 }
