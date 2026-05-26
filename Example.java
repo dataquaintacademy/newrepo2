566f31b907b4e9f3c8e4c688b87d66aee2000763
SravaniTechnologies <sravanitechworld@gmail.com>
2026-05-26 10:52:06 +0530

    new commit'

--- Example.java
+++ Example.java
@@ -4,5 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("Hello world");
 System.out.println("welcome to the world of DevOPs");
+System.out.printl("I'm new to to development");
 }
 }
