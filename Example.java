3e9e1b4a1d70a0d74a4ad5a96657241c82c4cabd
dataquaintacademy <dataquaintacademy@gmail.com>
2026-05-26 05:24:10 +0000

    new commit - fixed the issue with println statement

--- Example.java
+++ Example.java
@@ -4,6 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("Hello world");
 System.out.println("welcome to the world of DevOPs");
-System.out.printl("I'm new to to development");
+System.out.println("I'm new to to development");
 }
 }
